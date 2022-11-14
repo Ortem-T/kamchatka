5915f4b886c7ee690b43026218d4cdaa39b0b0da
Ortem-T <84287305+Ortem-T@users.noreply.github.com>
2022-11-14 22:01:00 +0200

    feat: create form

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -4,6 +4,7 @@ import styles from './Booking.module.css';
 import facebook from '../../images/facebook_bl.svg';
 import instagram from '../../images/instagram_bl.svg';
 import vk from '../../images/vk_bl.svg';
+import magazine from '../../images/magazine_kamchatka.png';
 import Form from '../Form/Form';
 
 function Booking() {
@@ -13,37 +14,51 @@ function Booking() {
         <div className={styles.content}>
           <h2 className={styles.title}>Онлайн-заявка</h2>
           <ul className={styles.social}>
-              <li>
-                <a
-                  href='https://ortem-t.github.io/russian-travel/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img className={styles.social_link} src={instagram} alt='instagram' />
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://ortem-t.github.io/russian-travel/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img className={styles.social_link} src={facebook} alt='facebook' />
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://ortem-t.github.io/russian-travel/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img className={styles.social_link} src={vk} alt='vk' />
-                </a>
-              </li>
-            </ul>
+            <li>
+              <a
+                href='https://ortem-t.github.io/russian-travel/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img className={styles.social_link} src={instagram} alt='instagram' />
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://ortem-t.github.io/russian-travel/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img className={styles.social_link} src={facebook} alt='facebook' />
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://ortem-t.github.io/russian-travel/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img className={styles.social_link} src={vk} alt='vk' />
+              </a>
+            </li>
+          </ul>
         </div>
         <Form />
       </div>
+      <div className={styles.memo}>
+        <h3 className={`${styles.title} ${styles.title_memo}`}>Что нужно знать отправляясь на Камчатку?</h3>
+        <p className={styles.text_memo}>Получите памятку туриста просто оставив свой e-mail.</p>
+        <form className={styles.form_memo}>
+          <input
+            className={styles.input_memo}
+            placeholder='Ваш e-mail'
+            type='email'
+            id='email'
+          />
+          <button className={styles.button_memo}>Получить</button>
+        </form>
+      </div>
+      <img className={styles.memo_img} src={magazine} alt='magazine' />
     </section>
   );
 }
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -5,53 +5,90 @@ import styles from './Form.module.css';
 function Form() {
   return (
     <form className={styles.form} noValidate>
-      <div>
-        <label className='login__label'>Имя участника</label>
-        <input
-          className='login__input'
-          placeholder='Имя'
-          type='text'
-          id='name'
-        />
-        <label className='login__label'>Количество человек</label>
-        <input
-          className='login__input'
-          placeholder='1'
-          type='text'
-          id='peoples'
-        />
+      <div className={styles.form_data}>
+        <div className={styles.form_name}>
+          <label className={styles.label}>Имя участника</label>
+          <input
+            className={styles.input}
+            placeholder='Имя'
+            type='text'
+            id='name'
+          />
+        </div>
+        <div className={styles.form_name}>
+          <label className={styles.label}>Количество человек</label>
+          <input
+            className={styles.input}
+            placeholder='1'
+            type='number'
+            id='peoples'
+          />
+        </div>
       </div>
-      <div>
-        <input
-          className='checkbox__input'
-          type='checkbox'
-          id='checkbox'
-          name='checkbox'
-          aria-label='Найти короткометражки'
-        />
-        <label className='checkbox__label' htmlFor='checkbox'>
-          Короткометражки
-        </label>
+      <div className={styles.form_rate}>
+        <p className={styles.label}>Тариф</p>
+        <ul className={styles.price_list}>
+          <li className={styles.price_item}>
+            <div>
+              <input
+                className={styles.input_checkbox}
+                type='radio'
+                id='base'
+                value='base'
+                name='rate'
+              />
+              <label className={styles.price_text} htmlFor='base'>Базовый (для камчадал)</label>
+            </div>
+            <p className={styles.price_cost}>79 000 руб</p>
+          </li>
+          <li className={styles.price_item}>
+            <div>
+              <input
+                className={styles.input_checkbox}
+                type='radio'
+                id='base+'
+                value='base+'
+                name='rate'
+              />
+              <label className={styles.price_text} htmlFor='base+'>Базовый + проживание</label>
+            </div>
+            <p className={styles.price_cost}>99 000 руб</p>
+          </li>
+          <li className={styles.price_item}>
+            <div>
+            <input
+              className={styles.input_checkbox}
+              type='radio'
+              id='base++'
+              value='base++'
+              name='rate'
+            />
+            <label className={styles.price_text} htmlFor='base++'>Базовый + проживание + долина гейзеров</label>
+            </div>
+            <p className={styles.price_cost}>179 000 руб</p>
+          </li>
+        </ul>
       </div>
-      <div>
-      <label className='login__label'>Имя участника</label>
+      <div className={styles.form_date}>
+        <label className={styles.label}>Даты тура</label>
         <input
-          className='login__input'
-          placeholder='Имя'
+          className={styles.input}
+          placeholder='23.06 - 04.07'
           type='date'
           id='name'
         />
       </div>
-      <div>
-      <label className='login__label'>Имя участника</label>
+      <div className={styles.form_comment}>
+        <label className={styles.label}>Комментарий</label>
         <textarea
-          className='login__input'
-          placeholder='Имя'
+          className={`${styles.input} ${styles.input_comment}`}
+          placeholder='Напишите ваш комментарий здесь'
           id='name'
+          rows={1}
         />
       </div>
       <button
-        className='login__button'
+        className={styles.button}
         type='submit'
       >
         Отправить заявку
